9998b1f559d07165522b1f58b457a542a3c7e939
nothingdev <nothingtillsomething@gmail.com>
2023-06-10 17:00:03 +0100

    Fixed mobile hamburger colour

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -43,6 +43,7 @@ import {
             display={{ base: 'flex', md: 'none' }}>
             <IconButton
               onClick={onToggle}
+              color={'white'}
               icon={
                 isOpen ? <CloseIcon w={3} h={3} /> : <HamburgerIcon w={5} h={5} />
               }
